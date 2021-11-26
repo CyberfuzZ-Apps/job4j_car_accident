d3be944454dcf263c48940b9e00ec7a8ef3ed628
CyberfuzZ <keepthelight@icloud.com>
2021-11-26 16:11:43 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 /**
  * Класс IndexControl
  *
@@ -15,6 +17,7 @@ public class IndexControl {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("list", List.of("First", "Second", "Third"));
         return "index";
     }
 }
